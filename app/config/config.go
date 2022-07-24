1882b6175467531e8bfab0e47db88086412c2a27
Bartosz Wyporkiewicz <nix.noxi379@gmail.com>
2022-07-24 11:16:27 +0200

    Features (#4)
    * Add Dockerfile
    * Add simple validation of environment variables
    * Expand README.md

--- app/config/config.go
+++ app/config/config.go
@@ -5,7 +5,7 @@ import "github.com/caarlos0/env/v6"
 type Config struct {
 	Symbol         string `env:"SYMBOL" envDefault:"btcusdt"`
 	QuantityToSell string `env:"QUANTITY_TO_SELL" envDefault:"25.0"`
-	MinimumBid     string `env:"MINIMUM_BID" envDefault:"42.0"`
+	MinimumBid     string `env:"MINIMUM_BID" envDefault:"2200.0"`
 }
 
 func New() (*Config, error) {
